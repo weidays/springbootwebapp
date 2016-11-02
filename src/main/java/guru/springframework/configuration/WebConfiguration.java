f8e580bb97de88f1bb0e9cb2cfe6c7a2cb8eaa3f
weidays <wei.huang@mail.dealmoon.com>
2016-11-02 17:06:56 +0800

    yes it is

--- src/main/java/guru/springframework/configuration/WebConfiguration.java
+++ src/main/java/guru/springframework/configuration/WebConfiguration.java
@@ -1,6 +1,6 @@
 package guru.springframework.configuration;
 
-import org.h2.server.web.WebServlet;
+
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.Configuration;
 public class WebConfiguration {
     @Bean
     ServletRegistrationBean h2servletRegistration(){
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
-        registrationBean.addUrlMappings("/console/*");
-        return registrationBean;
+        return null;
     }
 }
